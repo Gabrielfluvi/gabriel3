f47b0f9172555d3a8a8b179a77cdb606b2bc2c99
Gabrielfluvi <gabrielfluvisaro@gmail.com>
2024-11-28 08:31:04 -0300

    Add files via upload

---
+++ alura/assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sim em Inglês?',
+    'Oi em ingles é yes'
+)
